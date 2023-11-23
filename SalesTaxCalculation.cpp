1dba93c4cbc9f71d18954b178a189a059ceb2c7c
Clinton Ogada <clintonogada@gmail.com>
2023-11-23 19:41:47 +0300

    Update SalesTaxCalculation.cpp

--- SalesTaxCalculation.cpp
+++ SalesTaxCalculation.cpp
@@ -3,15 +3,17 @@ using namespace std;
 int main()
 {
  double Sales = 95000;
- const double StateTaxRate = .04; 
+ const double StateTaxRate = .04;
  const double CountyTaxRate = .02;
  double StateTax = Sales * StateTaxRate ;
  double CountyTax = Sales * CountyTaxRate;
  double Balance = Sales - (StateTax + CountyTax);
- cout<<"Your Total Sales is:"<<Sales<<endl;
- cout<<"Your State Tax is:"<<StateTax<<endl;
- cout<<"Your County Tax is:"<<CountyTax<<endl;
- cout<<"Your balance is: "<<Balance<<endl;
+ double totalTax = StateTax + CountyTax;
+ cout<<"Your Total Sales is: $"<<Sales<<endl;
+ cout<<"Your State Tax is: $"<<StateTax<<endl;
+ cout<<"Your County Tax is: $"<<CountyTax<<endl;
+ cout<<"Your Total Tax is: $"<<totalTax<<endl;
+ cout<<"Your balance is: $"<<Balance<<endl;
 
  return 0;
 }
