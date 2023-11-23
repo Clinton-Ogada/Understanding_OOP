0609dffe54857f4b12eba743d989c1cc990b17dc
Clinton Ogada <clintonogada@gmail.com>
2023-11-24 01:03:57 +0300

    Create circleAreaCalculator.cpp
    This program calculates the area of a circle. It prompts the user to enter the radius of the circle and then displays the corresponding area of the circle.

---
+++ circleAreaCalculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+int main() {
+    double radius;
+    cout<<"Enter radius";
+    cin>>radius;
+    const double pi = 3.142;
+    double area = pi * pow(radius, 2);
+    cout<<celsius;
+
+    return 0;
+}
