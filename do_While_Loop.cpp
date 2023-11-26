54481fbb2232f5d0c18ce71ea983634a0f07fda2
Clinton Ogada <clintonogada@gmail.com>
2023-11-26 20:48:01 +0300

    Create do_While_Loop.cpp

---
+++ do_While_Loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+int yourNum = 0;
+do { cout << "Enter +ve No. to CONTINUE or –ve No. to STOP ";
+cin >> yourNum;
+cout << yourNum << endl;
+} while (yourNum >=0) ;
+return 0;
+}
