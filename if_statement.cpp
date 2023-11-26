357ff85dd29910ba6d31040417487a22cb97f22f
Clinton Ogada <clintonogada@gmail.com>
2023-11-26 20:49:10 +0300

    Create if_statement.cpp

---
+++ if_statement.cpp
@@ -0,0 +1,13 @@
+#include<>
+using namespace std
+int main(){
+int a, b;
+cout<<"Enter value of a:\t";
+cin>>a;
+cout<<"Enter the value of b:\t";
+cin>>b;
+if(a<b){
+cout<<"a="<<a<<\n<<"b="<<b<<endl;
+cout<<"A is smaller than B"}
+}
+
